121f2c4f2c5729b67aaa7c926ea22fb8b636a71c
Coded76 <0xhope66@example.com>
2025-04-28 02:02:34 +0100

    update

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import FAQSection from "@/components/FaqSection";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/Hero";
 import Hero from "@/components/Hero";
+import MegaLaunch from "@/components/MegaLaunch";
 import Navbar from "@/components/Navbar";
 import TokenAllocation from "@/components/TokenAllocation";
 import WhatIsMega from "@/components/WhatIsMega";
@@ -15,14 +16,13 @@ export default function Home() {
       {/* <Hero />/ */}
       <HeroSection />
       <WhatIsMega />
+      <MegaLaunch />
       <ContractAddressSection />
       <TokenAllocation />
       <FAQSection />
       <Footer />
 
-    {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"> */}
-     
-
+      {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"> */}
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,33 +12,33 @@ export default function Navbar() {
     <nav className="bg-black text-white px-6 py-4 flex justify-between items-center">
       {/* Logo */}
       <div className="md:hidden flex"></div>
-   <div className="flex items-center gap-[90px]">
-   <div className="flex items-center space-x-2">
-        <div className="">
-          <img src="/navbar/logo.svg" alt="" />
+      <div className="flex items-center gap-[90px]">
+        <div className="flex items-center space-x-2">
+          <div className="">
+            <img src="/navbar/logo.svg" alt="" />
+          </div>
+          <span className="font-semibold text-lg">MEGA</span>
         </div>
-        <span className="font-semibold text-lg">MEGA</span>
-      </div>
 
-      {/* Desktop Menu */}
-      <div className="hidden md:flex space-x-8 items-center">
-        <a href="#what" className="hover:text-[#E3E354]">
-          What is MEGA?
-        </a>
-        <a href="#token" className="hover:text-[#E3E354]">
-          The Token
-        </a>
-        <a href="#allocation" className="hover:text-[#E3E354]">
-          Allocation
-        </a>
-        <a href="#contact" className="hover:text-[#E3E354]">
-          Contact Us
-        </a>
-        <a href="#buy" className="text-[#E3E354] font-bold">
-          +BUY NOW
-        </a>
+        {/* Desktop Menu */}
+        <div className="hidden md:flex space-x-8 items-center">
+          <a href="#what" className="hover:text-[#E3E354]">
+            What is MEGA?
+          </a>
+          <a href="#token" className="hover:text-[#E3E354]">
+            The Token
+          </a>
+          <a href="#allocation" className="hover:text-[#E3E354]">
+            Allocation
+          </a>
+          <a href="#contact" className="hover:text-[#E3E354]">
+            Contact Us
+          </a>
+          <a href="#buy" className="text-[#E3E354] font-bold">
+            +BUY NOW
+          </a>
+        </div>
       </div>
-   </div>
       <button className="ml-4 md:block hidden bg-gradient-to-b from-[#E3D7AE] to-[#E3E354] text-black px-4 py-2 rounded-md font-semibold">
         CONNECT WALLET
       </button>
@@ -52,34 +52,38 @@ export default function Navbar() {
 
       {/* Mobile Sidebar */}
       {isOpen && (
-        <div className="absolute top-0 right-0 w-64 h-full bg-black text-white flex flex-col p-6 space-y-6 border-l border-yellow-400 z-50">
-          <button onClick={toggleMenu} className="self-end">
+        <div className="absolute top-0 right-0 w-64 h-[488px] bg-black text-white flex flex-col p-6 space-y-6 border-l border-b rounded-bl-md border-[#E3E354] z-50">
+          <button onClick={toggleMenu} className="self-start">
             <X size={28} />
           </button>
 
-          <a href="#what" className="hover:bg-yellow-400 py-2 px-4 rounded">
+          <a
+            href="#what"
+            className="bg-gradient-to-b from-[#E3D7AE] to-[#E3E354] py-2 text-black text-center rounded"
+          >
             What is MEGA?
           </a>
-          <a href="#token" className="hover:text-yellow-400">
+          <a href="#token" className="hover:text-[#E3E354] text-center">
             The Token
           </a>
-          <a href="#allocation" className="hover:text-yellow-400">
+          <a href="#allocation" className="hover:text-[#E3E354] text-center">
             Allocation
           </a>
-          <a href="#contact" className="hover:text-yellow-400">
+          <a href="#contact" className="hover:text-[#E3E354] text-center">
             Contact Us
           </a>
-          <a href="#buy" className="text-yellow-400 font-bold">
+          <a href="#buy" className="text-[#E3E354] font-bold text-center">
             +BUY NOW
           </a>
 
-          <button className="bg-yellow-400 text-black px-4 py-2 rounded-md font-semibold">
+          <button className="bg-[#E3E354] text-black px-4 py-2 rounded-md font-semibold">
             CONNECT WALLET
           </button>
 
-          <div className="flex items-center space-x-2 pt-10">
-            <div className="border border-yellow-400 rounded-full p-1">
-              <span className="text-yellow-400 font-bold text-lg">M</span>
+          <div className="flex items-center justify-center space-x-2 pt-6">
+            <div className="">
+              <img src="/navbar/logo.svg" alt="" />
+              {/* <span className="text-[#E3E354] font-bold text-lg">M</span> */}
             </div>
             <span className="font-semibold text-lg">MEGA</span>
           </div>
